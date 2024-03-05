e314605d343476bf1615b89415413062490ef29f
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-05 12:04:32 +0900

    キャンプ場のモデルの作成

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+mongoose
+    .connect("mongodb://localhost:27017/yelp-camp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log("MongoDB コネクションOK");
+    })
+    .catch((err) => {
+        console.log("MongoDB コネクションエラー");
+        console.log(err);
+    });
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({ title: "私の庭", description: "気軽に安くキャンプ" });
+    await camp.save();
+    res.send(camp);
 });
 
 app.listen(3000, () => {
