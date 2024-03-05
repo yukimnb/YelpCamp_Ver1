e1f7d0d85aa7cc1fec9ff4955e3334a85ad5cafc
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-05 15:00:03 +0900

    キャンプ場の編集

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 mongoose
     .connect("mongodb://localhost:27017/yelp-camp", {
@@ -21,8 +22,11 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Middleware ----------------------------------------
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
+// GET ----------------------------------------
 app.get("/", (req, res) => {
     res.render("home");
 });
@@ -41,9 +45,22 @@ app.get("/campgrounds/:id", async (req, res) => {
     res.render("campgrounds/show", { campground });
 });
 
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+});
+
+// POST ----------------------------------------
 app.post("/campgrounds", async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+// PUT ----------------------------------------
+app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
