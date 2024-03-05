b34ac13ae47af74d12d6806a33fbdde6125fba6c
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-05 22:54:38 +0900

    ejs-mate導入

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const app = express();
+
 mongoose
     .connect("mongodb://localhost:27017/yelp-camp", {
         useNewUrlParser: true,
@@ -17,8 +20,7 @@ mongoose
         console.log(err);
     });
 
-const app = express();
-
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
