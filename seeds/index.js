aec90abe691a345fa3c3b48a81d05edc59789ccb
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-05 13:03:01 +0900

    キャンプ場のデータをシード

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+mongoose
+    .connect("mongodb://localhost:27017/yelp-camp", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log("MongoDB コネクションOK");
+    })
+    .catch((err) => {
+        console.log("MongoDB コネクションエラー");
+        console.log(err);
+    });
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const randomCityIndex = Math.floor(Math.random() * cities.length);
+        const camp = new Campground({
+            location: `${cities[randomCityIndex].prefecture}${cities[randomCityIndex].city}`,
+            title: `${sample(descriptors)} - ${sample(places)}`,
+        });
+        await camp.save();
+    }
+};
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
