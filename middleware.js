42de695868da8c7d519234e866b1266c018acf8c
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-11 15:00:45 +0900

    レビューに権限を付与

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const ExpressError = require("./utils/ExpressError");
 const { campgroundSchema, reviewSchema } = require("./schemas");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -14,6 +15,16 @@ module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(req.params.id);
     if (!campground.author.equals(req.user._id)) {
+        req.flash("error", "そのアクションの権限がありません");
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await reviewId.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
         req.flash("error", "そのアクションの権限がありません");
         return res.redirect(`/campgrounds/${id}`);
     }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -20,8 +20,14 @@ router.get("/new", isLoggedIn, (req, res) => {
 router.get(
     "/:id",
     catchAsync(async (req, res) => {
-        const campground = await Campground.findById(req.params.id).populate("reviews").populate("author");
-        console.log(campground);
+        const campground = await Campground.findById(req.params.id)
+            .populate({
+                path: "reviews",
+                populate: {
+                    path: "author",
+                },
+            })
+            .populate("author");
         if (!campground) {
             req.flash("error", "キャンプ場は見つかりませんでした");
             return res.redirect("/campgrounds");
