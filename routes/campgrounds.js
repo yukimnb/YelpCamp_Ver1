17c8c28fe5282c28b6425f21f211024470f75ab1
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-11 15:24:53 +0900

    キャンプ場のコントローラーを作成

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,100 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
 // GET ----------------------------------------
-router.get(
-    "/",
-    catchAsync(async (req, res) => {
-        const campgrounds = await Campground.find({});
-        res.render("campgrounds/index", { campgrounds });
-    })
-);
-
-router.get("/new", isLoggedIn, (req, res) => {
-    res.render("campgrounds/new");
-});
-
-router.get(
-    "/:id",
-    catchAsync(async (req, res) => {
-        const campground = await Campground.findById(req.params.id)
-            .populate({
-                path: "reviews",
-                populate: {
-                    path: "author",
-                },
-            })
-            .populate("author");
-        if (!campground) {
-            req.flash("error", "キャンプ場は見つかりませんでした");
-            return res.redirect("/campgrounds");
-        }
-        res.render("campgrounds/show", { campground });
-    })
-);
-
-router.get(
-    "/:id/edit",
-    isLoggedIn,
-    isAuthor,
-    catchAsync(async (req, res) => {
-        const { id } = req.params;
-        const campground = await Campground.findById(id);
-        if (!campground) {
-            req.flash("error", "キャンプ場は見つかりませんでした");
-            return res.redirect("/campgrounds");
-        }
-        res.render("campgrounds/edit", { campground });
-    })
-);
+router.get("/", catchAsync(campgrounds.index));
+router.get("/new", isLoggedIn, campgrounds.renderNewForm);
+router.get("/:id", catchAsync(campgrounds.showCampground));
+router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
 // POST ----------------------------------------
-router.post(
-    "/",
-    isLoggedIn,
-    validateCampground,
-    catchAsync(async (req, res) => {
-        const campground = new Campground(req.body.campground);
-        campground.author = req.user._id;
-        await campground.save();
-        req.flash("success", "新しいキャンプ場を登録しました");
-        res.redirect(`/campgrounds/${campground._id}`);
-    })
-);
+router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 // PUT ----------------------------------------
-router.put(
-    "/:id",
-    isLoggedIn,
-    isAuthor,
-    validateCampground,
-    catchAsync(async (req, res) => {
-        const { id } = req.params;
-        const campground = await Campground.findById(id);
-        if (!campground.author.equals(req.user._id)) {
-            req.flash("error", "更新する権限がありません");
-            return res.redirect(`/campgrounds/${id}`);
-        }
-        const camp = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-        req.flash("success", "キャンプ場を更新しました");
-        res.redirect(`/campgrounds/${camp._id}`);
-    })
-);
+router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
 
 // DELETE ----------------------------------------
-router.delete(
-    "/:id",
-    isLoggedIn,
-    isAuthor,
-    catchAsync(async (req, res) => {
-        const { id } = req.params;
-        await Campground.findByIdAndDelete(id);
-        req.flash("success", "キャンプ場を削除しました");
-        res.redirect("/campgrounds");
-    })
-);
+router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
