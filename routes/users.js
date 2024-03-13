ce6904177acb31eb8bc608e2a50081aeb7ff1f56
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-13 09:29:58 +0900

    ユーザーのコントローラーを作成

--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const User = require("../models/user");
+const users = require("../controllers/users");
 
 // GET ----------------------------------------
-router.get("/register", (req, res) => {
-    res.render("users/register");
-});
+router.get("/register", users.renderRegister);
 
-router.get("/login", (req, res) => {
-    res.render("users/login");
-});
+router.get("/login", users.renderLogin);
 
-router.get("/logout", (req, res, next) => {
-    req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash("success", "ログアウトしました");
-        res.redirect("/campgrounds");
-    });
-});
+router.get("/logout", users.logout);
 
 // POST ----------------------------------------
-router.post("/register", async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registerdUser = await User.register(user, password);
-        req.login(registerdUser, (err) => {
-            if (err) {
-                return next(err);
-            }
-            req.flash("success", "Yelp Campへようこそ");
-            res.redirect("/campgrounds");
-        });
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("/register");
-    }
-});
+router.post("/register", users.register);
 
-router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), (req, res) => {
-    req.flash("success", "ログインしました");
-    res.redirect("/campgrounds");
-});
+router.post("/login", passport.authenticate("local", { failureFlash: true, failureRedirect: "/login" }), users.login);
 
 module.exports = router;
