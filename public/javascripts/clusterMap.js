9681b3bad3a31987dabcf8de3308c3f20cbb20b8
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-14 22:58:41 +0900

    地図の表示をスタイリング

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,11 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: "map",
+    container: "cluster-map",
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: "mapbox://styles/mapbox/outdoors-v12",
     center: [138, 39],
     zoom: 3,
 });
+map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 map.on("load", () => {
     // Add a new source from our GeoJSON data and
     // set the 'cluster' option to true. GL-JS will
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -5,7 +5,7 @@ const map = new mapboxgl.Map({
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 8, // starting zoom
 });
-
+map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h5>${campground.title}</h5><p>${campground.location}</p>`))
