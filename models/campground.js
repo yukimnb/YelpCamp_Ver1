707bf734d95e3697eff301c3653bcd53d4e02f78
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-14 15:09:10 +0900

    クラスターにポップアップを追加

--- models/campground.js
+++ models/campground.js
@@ -2,38 +2,48 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const review = require("./review");
 
-const campgroundSchema = new Schema({
-    title: String,
-    geometry: {
-        type: {
-            type: String,
-            enum: ["Point"],
-            required: true,
+const opts = { toJSON: { virtuals: true } };
+const campgroundSchema = new Schema(
+    {
+        title: String,
+        geometry: {
+            type: {
+                type: String,
+                enum: ["Point"],
+                required: true,
+            },
+            coordinates: {
+                type: [Number],
+                required: true,
+            },
         },
-        coordinates: {
-            type: [Number],
-            required: true,
-        },
-    },
-    price: Number,
-    description: String,
-    location: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-    },
-    reviews: [
-        {
+        price: Number,
+        description: String,
+        location: String,
+        author: {
             type: Schema.Types.ObjectId,
-            ref: "Review",
+            ref: "User",
         },
-    ],
-    images: [
-        {
-            url: String,
-            filename: String,
-        },
-    ],
+        reviews: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Review",
+            },
+        ],
+        images: [
+            {
+                url: String,
+                filename: String,
+            },
+        ],
+    },
+    opts
+);
+
+campgroundSchema.virtual("properties.popUpMarkup").get(function () {
+    return `<strong><a href="/campgrounds/${
+        this._id
+    }">${this.title}</a></strong><p>${this.description.substring(0, 20)}...</p>`;
 });
 
 campgroundSchema.post("findOneAndDelete", async function (doc) {
--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -6,7 +6,6 @@ const map = new mapboxgl.Map({
     center: [138, 39],
     zoom: 3,
 });
-
 map.on("load", () => {
     // Add a new source from our GeoJSON data and
     // set the 'cluster' option to true. GL-JS will
@@ -85,9 +84,8 @@ map.on("load", () => {
     // the location of the feature, with
     // description HTML from its properties.
     map.on("click", "unclustered-point", (e) => {
+        const { popUpMarkup } = e.features[0].properties;
         const coordinates = e.features[0].geometry.coordinates.slice();
-        const mag = e.features[0].properties.mag;
-        const tsunami = e.features[0].properties.tsunami === 1 ? "yes" : "no";
 
         // Ensure that if the map is zoomed out such that
         // multiple copies of the feature are visible, the
@@ -96,10 +94,7 @@ map.on("load", () => {
             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
 
-        new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(`magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`)
-            .addTo(map);
+        new mapboxgl.Popup().setLngLat(coordinates).setHTML(popUpMarkup).addTo(map);
     });
 
     map.on("mouseenter", "clusters", () => {
